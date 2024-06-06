cc0053f79da16437296c3de0d52373773adb6afa
ASUS <rikardoperez0809@gmail.com>
2024-06-06 08:45:33 -0500

    Se genera un documento en el escritorio

--- RP/MainPage.xaml.cs
+++ RP/MainPage.xaml.cs
@@ -1,17 +1,22 @@
-﻿namespace RP
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace RP
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
+       
         private async void Recargar_Clicked(object sender, EventArgs e)
         {
-           
+          
             int monto = 0;
             if (radioButton3RP.IsChecked)
                 monto = 3;
@@ -20,21 +25,32 @@
             else if (radioButton10RP.IsChecked)
                 monto = 10;
 
-           
+            // Confirmación de recarga
             bool confirmacion = await DisplayAlert("Confirmación", $"¿Desea recargar ${monto} al número {txtPhoneNumber.Text} con el operador {pickerOperator.SelectedItem}?", "Sí", "No");
 
-            
+           
             if (confirmacion)
             {
-                await RealizarRecarga();
+                await RealizarRecarga(monto);
             }
         }
 
-       
-        private async Task RealizarRecarga()
+        
+        private async Task RealizarRecarga(int monto)
         {
-            
+           
             await Task.Delay(2000); 
+
+          
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            string fileName = $"{txtPhoneNumber.Text}.txt";
+            string filePath = Path.Combine(desktopPath, fileName);
+
+            string textoArchivo = $"Se hizo una recarga de ${monto} dólares en la siguiente fecha: {DateTime.Now}";
+
+            File.WriteAllText(filePath, textoArchivo);
+
+          
             await DisplayAlert("Recarga Exitosa", "La recarga se ha realizado exitosamente.", "OK");
         }
     }
